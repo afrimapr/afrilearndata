822a3bb517d35a03f1715566ebea3ebef52b6b33
AndySouth <southandy@gmail.com>
2020-10-28 09:22:09 +0000

    start man pages

---
+++ R/sfafrihway.r
@@ -0,0 +1,29 @@
+#' @name sfafrihway
+#' @aliases sflines
+#' @title African trans-continental highway network lines
+#'
+#' @description The object loaded is a \code{sf} object containing simplified lines of planned transcontinental highway
+#'
+#' @format Formal class 'sf' [package "sf"];
+#' \itemize{
+#'     \item{name_long} {character vector of country names}
+#'     \item{geom} {sfc_MULTIPOLYGON}
+#' }
+#' The object is in geographical coordinates using the WGS84 datum.
+#'
+#' @seealso
+#' https://en.wikipedia.org/wiki/Trans-African_Highway_network
+#'
+#' @source \url{https://www.google.com/maps/d/u/0/viewer?msa=0&mid=1nEU2oBFzSxabx3Z14nTyZP3KSzY&ll=1.9249940151081273%2C12.874260000000021&z=3}
+#' @docType data
+#' @keywords datasets sf
+#' @examples
+#' if (requireNamespace("sf", quietly = TRUE)) {
+#'   library(sf)
+#'   data(sfafrihway)
+#'   # or
+#'   sfafrihway <- sf::read_sf("inst/extdata/Trans-African Highway Network.kml")
+#'
+#'   plot(sf::st_geometry(sfafrihway))
+#' }
+"sfafrihway"
