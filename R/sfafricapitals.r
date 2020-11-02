775536ae2b804384a5d914ff42c56b2a5785d534
AndySouth <southandy@gmail.com>
2020-11-02 18:53:38 +0000

    data documentation

---
+++ R/sfafricapitals.r
@@ -0,0 +1,30 @@
+#' @name sfafricapitals
+#' @aliases sfpoints
+#' @title African capital city points
+#'
+#' @description a \code{sf} object points of African capital cities
+#'
+#' @format Formal class 'sf' [package "sf"];
+#' \itemize{
+#'     \item{name} {character vector capital city names}
+#'     \item{countryname} {character vector country names}
+#'     \item{pop} {numeric estimated population}
+#'     \item{iso3c} {character vector ISO 3 letter country code}
+#'     \item{geometry} {sfc_POINT}
+#' }
+#' The object is in geographical coordinates using the WGS84 datum.
+#'
+#'
+#' @source \url{https://cran.r-project.org/web/packages/maps/}
+#' @docType data
+#' @keywords datasets sf
+#' @examples
+#' if (requireNamespace("sf", quietly = TRUE)) {
+#'   library(sf)
+#'   data(sfafricapitals)
+#'   # or
+#'   sfafricapitals <- sf::read_sf("inst/extdata/africapitals.gpkg")
+#'
+#'   plot(sf::st_geometry(sfafricapitals))
+#' }
+"sfafricapitals"
